b5fccd8bb58b1c7fbfbb6ff59546a2f95776cf57
XEVON <87572659+XEVON01@users.noreply.github.com>
2022-01-17 10:01:28 +0530

    Add files via upload

---
+++ simple program for login.py
@@ -0,0 +1,11 @@
+real_username=("name") #username you want to be added for login
+username=input ("enter your username") 
+if username==real_username :
+    real_password=("0000") #type any password you want for login
+    password=input ("enter your password")
+    if password==real_password :
+        print("your codeis: rtt457") #it's like a code provided you can also type a welcome message
+    else:
+            print("wrong password")
+else:
+    print("no usernames found")
